54a0544e668bd0fd164fddf20fcd60f81518f82f
ahrazr-cmyk <ahraz_r@cs.iitr.ac.in>
2026-04-29 01:57:17 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const mqtt = require("mqtt");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// MQTT CONFIG
+const client = mqtt.connect("mqtts://f1108f1ecd8140f98e0b481a54f10251.s1.eu.hivemq.cloud:8883", {
+    username: "esp32",
+    password: "123456"
+});
+
+client.on("connect", () => {
+    console.log("✅ MQTT Connected");
+    client.subscribe("vehicle/data");
+});
+
+client.on("message", (topic, message) => {
+    const data = message.toString();
+    console.log("📩 Received:", data);
+    io.emit("data", data);
+});
+
+// Serve frontend
+app.use(express.static("public"));
+
+// IMPORTANT for Railway/Render
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("🚀 Dashboard running on port", PORT);
+});
